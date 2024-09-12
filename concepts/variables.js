39d2e921f4b3a61a0273145a15290f75373923a7
Brian <dev.brianoctavio@gmail.com>
2024-09-12 13:56:50 -0600

    03 varibles let y const

---
+++ concepts/variables.js
@@ -0,0 +1,18 @@
+//Declaración de variables con let y const
+//var: en desuso
+
+//variables let: cambiarán de valor
+let age; //declaración
+console.log(age);
+age = 23; //inicialización
+console.log(age);
+
+let name = "Brian"; //declaración e inicialización
+console.log(name);
+name = "Tavo";
+console.log(name);
+
+//constantes const: no cambiarán de valor
+const PI = 3.1416;
+//PI = 3.15;
+console.log(PI);
