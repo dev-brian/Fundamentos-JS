c293607eb7bf17d2bae37b3ac29e40351ea54f7f
Brian <dev.brianoctavio@gmail.com>
2024-09-12 13:44:56 -0600

    02 Operadores

---
+++ concepts/operator.js
@@ -0,0 +1,32 @@
+//operadores matematicos
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+//operadores de asignacion
+let a = 10
+a +=5; //a=a+5
+a -=5;
+a *=5;
+a /=5;
+a %=5;
+a **=5;
+console.log(a)
+
+//operadores de comparacion
+console.log(2>2);
+console.log(2<2);
+console.log(2>=2);
+console.log(2<=2);
+console.log(2===2);
+console.log(2==2);
+console.log(2!=2);
+console.log(2!==2);
+
+//operadores logicos
+console.log(true&&true); //and
+console.log(true||true); //or
+console.log(!true); //not
