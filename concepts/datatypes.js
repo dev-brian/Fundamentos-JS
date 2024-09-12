d038e1f2b91f6baa472d336b1d2f8d328e5f04b3
Brian <dev.brianoctavio@gmail.com>
2024-09-12 13:36:25 -0600

    Tipos de datos JavaScript

---
+++ concepts/datatypes.js
@@ -0,0 +1,22 @@
+//Tipos de datos en JavaScript
+//Numericos (enteros y decimales)
+console.log(23)
+console.log(typeof 23)
+console.log(3.1416)
+console.log(typeof 3.1416)
+
+//booleanos (true, false)
+console.log(true)
+console.log(typeof true)
+console.log(false)
+console.log(typeof false)
+
+//tipo string
+console.log("hello world")
+console.log(typeof "hello world")
+
+console.log(undefined)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof [1,2,3])
+console.log(typeof {name: 'John'})
